f65497a5fc9589d8af41e8181bffe9d17ee51ac2
Sergei <asvmos@yandex.ru>
2018-12-23 14:02:31 +0300

    Yandex backup script modified

--- yandex.backup.workspace.sh
+++ yandex.backup.workspace.sh
@@ -3,16 +3,18 @@ workspace_path="/home/smdslim/Workspace"
 temp_path="/media/smdslim/SECONDARY/Temp"
 remote_path="/Documents/$archive_name"
 obtain_url="https://cloud-api.yandex.net/v1/disk/resources/upload?path=$remote_path&overwrite=true"
+log_path="/home/smdslim/.logs/yandex.disk.log"
 
 # Archive workspace to temp folder
 echo "Making archive"
-notify-send 'Workspace backup: making archive'
+echo "$(date) Making archive" >> "$log_path"
+
 cd "$temp_path"
 # -FS to rewrite archive data
 zip -q -FSr "$archive_name" "$workspace_path"
 
 echo "Uploading archive"
-notify-send 'Workspace backup: uploading archive to Yandex.Disk'
+echo "$(date) Uploading archive" >> "$log_path"
 
 # Get upload link (OAuth key work 1 year - generated on 17.12.2018)
 url=$( curl -s -H "Authorization: OAuth AQAAAAAAp-U9AAVava7Wl90rPUn0ln9kDyhywA8" "$obtain_url" | python3 -c "import sys, json; print(json.load(sys.stdin)['href'])")
@@ -20,7 +22,7 @@ url=$( curl -s -H "Authorization: OAuth AQAAAAAAp-U9AAVava7Wl90rPUn0ln9kDyhywA8"
 curl -s "$url" --upload-file "$temp_path/$archive_name"
 
 echo "Archive uploaded!"
-notify-send 'Workspace backup: archive uploaded'
+echo "$(date) Archive uploaded!" >> "$log_path"
 
 # Go back to initial folder
 cd -
